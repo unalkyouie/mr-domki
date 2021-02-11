6066a8167e18aa41de17393c0bc435067d543fbf
Ola <alk.pikus@gmail.com>
2021-02-11 12:50:36 +0100

    chore: react native project init with typescrit config

---
+++ domki/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView></SafeAreaView>
+    </>
+  );
+};
+
+export default App;
