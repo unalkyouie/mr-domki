6577029bba97c6eeb14773549cf13a504cd6b17a
Ola <alk.pikus@gmail.com>
2021-02-11 16:45:25 +0100

    fix: fixed issues

--- domki/babel.config.js
+++ domki/babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        allowUndefined: true,
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+      },
+    ],
+  ],
   presets: ['module:metro-react-native-babel-preset'],
 };
